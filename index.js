2ffd4865e29718ff30ec8f2b1ee6d3c45d9c0bbd
kikkigenkai <felix116222@gmail.com>
2021-01-26 20:23:30 +0300

    done

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const sequelize = require('./utils/database');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const todoRoutes = require('./routes/todo');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use('/api/todo', todoRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile('/index.html');
+});
+
+(async () => {
+    try {
+        await sequelize.sync();   
+        app.listen(PORT);
+   } catch (err) {
+       console.log(err);
+   }
+})();
---
+++ routes/todo.js
@@ -0,0 +1,71 @@
+const { Router } = require('express');
+const Todo = require('../models/todo');
+const router = Router();
+
+
+
+router.get('/', async (req, res) => {
+    try {
+        const todos = await Todo.findAll();
+
+        res.status(200).json(todos);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'error'
+        });
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        const todo = await Todo.create({
+            title: req.body.title,
+            done: false
+        });
+
+        res.status(201).json({ todo });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'error'
+        });
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    try {
+        const todo = await Todo.findByPk(+req.params.id);
+
+        todo.done = req.body.done;
+        await todo.save();
+
+        res.status(200).json({todo});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'error'
+        });
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const todos = await Todo.findAll({
+            where: {
+                id: +req.params.id
+            }
+        });
+
+        await todos[0].destroy();
+
+        res.status(204).json({});
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: 'error'
+        });
+    }
+});
+
+module.exports = router;
